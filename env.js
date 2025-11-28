d20b0908fb8767360584a0c68fd1d5e9c0174e17
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2025-11-28 15:40:53 +0100

    validation 2

--- env.js
+++ env.js
@@ -3,7 +3,7 @@ const { cleanEnv, url, host, port, bool, str } = require("envalid");
 
 const env = cleanEnv(process.env, {
   PORT: port({ default: 7000 }),
-  HOST: host({ default: "http://localhost" }),
+  HOST: str({ default: "http://localhost" }),
   ENUMERATEUSERS: bool({ default: true }),
   DESTINATION: url(),
   METADATA: url(),
