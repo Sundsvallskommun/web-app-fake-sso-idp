2272c55108b273c190e2c1b64c912b852922c62c
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2025-11-28 15:33:42 +0100

    use host and port from env

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const app = create({
   basepath: env.BASEPATH,
   // NOTE: If false user will have to type in username and password themself.
   enumerateUsers: env.ENUMERATEUSERS,
+  host: env.HOST,
+  port: env.PORT,
   serviceProvider: {
     destination: env.DESTINATION,
-    metadata: env.METADATA
+    metadata: env.METADATA,
   },
-  users: users
-})
+  users: users,
+});
 
 app.listen(env.PORT)
